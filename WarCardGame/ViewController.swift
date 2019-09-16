ed466a96adc1fd376c77ca13337e25164d634f9f
Ebi Igweze <ebi.igweze@gmail.com>
2019-09-16 17:25:06 +0100

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by ebi igweze on 16/09/2019.
+//  Copyright © 2019 ebi igweze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
